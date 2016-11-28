cef67815465ab6f6d59cf1e3751c122240947b49
Stefan Walther <Stefan.Walther@qlik.com>
2016-11-28 02:13:10 +0100

    docs: Inline doc

--- src/profile.js
+++ src/profile.js
@@ -15,12 +15,26 @@ export default class Profile {
     this.logger = this.base.logger;
   }
 
+  /**
+   * Get the GitHub profile.
+   * @param options
+   * @param filter
+   * @param cb
+   * @private
+   */
   _getProfile( options, filter, cb ) {
     ghUtils.getAll( this.ghClient, 'users.get', options, ( err, res ) => {
       this.logger.silly( res );
 
       return cb( err, res );
     } )
+  }
+
+  /**
+   * Sync the profile with what have been stored so far.
+   */
+  sync() {
+    this.base.logger.silly( 'Just sync' );
   }
 
 }
