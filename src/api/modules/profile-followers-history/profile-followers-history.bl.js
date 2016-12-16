4652711fa4f177201ad96e2bed1f8ee18ae2cb00
Stefan Walther <Stefan.Walther@qlik.com>
2016-12-16 15:51:56 +0100

    feat: tests for profile-followers-history

--- src/api/modules/profile-followers-history/profile-followers-history.bl.js
+++ src/api/modules/profile-followers-history/profile-followers-history.bl.js
@@ -1,5 +1,7 @@
 import { Model as ProfileFollowersHistoryModel } from './profile-followers-history.model';
 import Context from './../../config/context';
+import _ from 'lodash';
+import Promise from 'bluebird';
 
 export default class ProfileFollowersHistoryBL {
   constructor( context ) {
@@ -21,8 +23,29 @@ export default class ProfileFollowersHistoryBL {
    */
   static ensure( data ) {
 
+    if ( _.isArray( data ) ) {
+
+      return Promise.map( data, ( item ) => {
+        return ProfileFollowersHistoryBL.ensureSingle( item )
+      } )
+
+    } else {
+      return ProfileFollowersHistoryBL.ensureSingle( data );
+    }
+  }
+
+  static ensureSingle( data ) {
+
+    //Todo: That's a hack, not sure how to tackle better => investigate
+    // (we will similar problems and patterns in other use cases)
+    // Could be that we have a static method on the schema to solve that problem
+    if ( !data.date_from ) {
+      data.date_from = new Date().setUTCHours( 0, 0, 0, 0 );
+    }
+
     let updateOpts = { new: true, upsert: true, setDefaultsOnInsert: true };
-    let query = { profile_id: data.profile_id, user_id: data.user_id };
+    let query = { profile_id: data.profile_id, user_id: data.user_id, date_from: data.date_from };
+
     return ProfileFollowersHistoryModel
       .findOneAndUpdate( query, data, updateOpts )
       .exec();
@@ -33,10 +56,9 @@ export default class ProfileFollowersHistoryBL {
     return ProfileFollowersHistoryModel
       .find( {
         profile_id: profileId,
-        date_to: null
+        date_to: { $exists: false }
       } )
       .exec();
-
   }
 
   static count() {
--- test/integration/profile-followers-history.bl.spec.js
+++ test/integration/profile-followers-history.bl.spec.js
@@ -4,7 +4,7 @@ import DBHelpers from './../lib/db-helpers';
 import _ from 'lodash';
 import Promise from 'bluebird';
 
-describe.only( 'profile-followers-history.bl', () => {
+describe( 'profile-followers-history.bl', () => {
 
   let dbHelpers;
   let context;
@@ -21,12 +21,16 @@ describe.only( 'profile-followers-history.bl', () => {
     ] );
   } );
 
-  it.only( 'has some methods', () => {
+  it( 'has some methods', () => {
     expect( ProfileFollowersHistoryBL ).to.have.a.property( 'ensure' );
+    expect( ProfileFollowersHistoryBL ).to.have.a.property( 'getActiveFollowersByProfile' );
+    expect( ProfileFollowersHistoryBL ).to.have.a.property( 'count' );
+    expect( ProfileFollowersHistoryBL ).to.have.a.property( 'countByProfileId' );
+    expect( ProfileFollowersHistoryBL ).to.have.a.property( 'removeByProfileId' );
     expect( ProfileFollowersHistoryBL ).to.have.a.property( 'removeAll' );
   } );
 
-  it.only( '`removeAll` removes all documents', () => {
+  it( '`removeAll` removes all documents', () => {
     return ProfileFollowersHistoryBL.removeAll()
       .then( ( result ) => {
         expect( result ).to.exist;
@@ -50,14 +54,15 @@ describe.only( 'profile-followers-history.bl', () => {
         expect( result ).to.have.a.property( 'date_from' ).to.exist;
         expect( result ).to.have.a.property( 'date_to' ).to.not.exist;
         expect( result ).to.have.a.property( 'last_check' ).to.exist;
-        return ProfileFollowersHistoryBL.countByProfileId( doc.profile_id )
-          .then( ( count ) => {
-            expect( count ).to.be.equal( 1 );
-          } )
       } )
+      .then( () => { return ProfileFollowersHistoryBL.countByProfileId( doc.profile_id ) })
+      .then( (count) => {
+        expect(count).to.be.equal(1);
+      })
+
   } );
 
-  it.only( '`save` does not create a new entry for same profile_id, user_id combination (if date_to is null)', () => {
+  it( '`save` does not create a new entry for same profile_id, user_id combination (if date_to is null)', () => {
 
     let doc1 = {
       profile_id: 1,
@@ -88,34 +93,144 @@ describe.only( 'profile-followers-history.bl', () => {
       } )
   } );
 
-  it( 'last_check gets updated on update', () => {
-    expect( false ).to.be.true;
+  it( '`ensure` => last_check gets updated on findOneAndUpdate', () => {
+    let doc1 = {
+      profile_id: 1,
+      user_id: 1,
+      date_from: new Date().setUTCHours( 0, 0, 0, 0 )
+    };
+    let doc2 = {
+      profile_id: 1,
+      user_id: 1,
+      date_from: new Date().setUTCHours( 0, 0, 0, 0 )
+    };
+
+    let vLastCheck1;
+    let vLastCheck2;
+    return ProfileFollowersHistoryBL.ensure( _.clone( doc1 ) )
+      .then( ( result ) => {
+        expect( result ).to.have.property( 'last_check' );
+        vLastCheck1 = result.last_check.getTime();
+      } )
+      .then( () => { return Promise.delay( 1000 ) } )
+      .then( () => { return ProfileFollowersHistoryBL.ensure( _.clone( doc2 ) ) } )
+      .then( ( result ) => {
+        expect( result ).to.have.property( 'last_check' );
+        vLastCheck2 = result.last_check.getTime();
+        expect( vLastCheck1 ).to.be.below( vLastCheck2 );
+      } )
+
   } );
 
-  it( '`create` creates a new entry for the same profile_id, user if date_to is not null', () => {
-    expect( true ).to.be.false;
+  it( '`ensure` creates a new entry for the same profile_id, user if date_to is not null', () => {
+    let dateToday = new Date();
+    let doc1 = {
+      profile_id: 1,
+      user_id: 1,
+      date_from: new Date( dateToday.setDate( dateToday.getDate() - 2 ) ).setUTCHours( 0, 0, 0, 0 ),
+      date_to: new Date( dateToday.setDate( dateToday.getDate() - 1 ) ).setUTCHours( 0, 0, 0, 0 )
+    };
+    let doc2 = {
+      profile_id: 2,
+      user_id: 1,
+      date_from: new Date().setUTCHours( 0, 0, 0, 0 )
+    };
+
+    return ProfileFollowersHistoryBL.ensure( _.clone( doc1 ) )
+      .then( () => { return ProfileFollowersHistoryBL.ensure( _.clone( doc2 ) ) } )
+      .then( () => { return ProfileFollowersHistoryBL.count() } )
+      .then( ( count ) => {
+        expect( count ).to.be.equal( 2 );
+      } )
   } );
 
-  it( '`create` can also handle multiple entries', () => {
-    expect( true ).to.be.false;
+  it( '`ensure` can also handle multiple entries', () => {
+
+    let doc1 = {
+      profile_id: 1,
+      user_id: 1,
+      date_from: new Date().setUTCHours( 0, 0, 0, 0 )
+    };
+    let doc2 = {
+      profile_id: 2,
+      user_id: 1,
+      date_from: new Date().setUTCHours( 0, 0, 0, 0 )
+    };
+
+    let docs = [];
+    docs.push( doc1 );
+    docs.push( doc2 );
+
+    return ProfileFollowersHistoryBL.ensure( docs )
+      .then( ( result ) => {
+        expect( result ).to.be.an.array;
+        expect( result ).to.be.of.length( 2 );
+      } )
+      .then( () => { return ProfileFollowersHistoryBL.count() } )
+      .then( ( count ) => {
+        expect( count ).to.be.equal( 2 );
+      } )
+
   } );
 
   it( '`removeByProfile` removes only the entries for the profile', () => {
-    expect( true ).to.be.false;
+    let doc1 = {
+      profile_id: 1,
+      user_id: 1,
+      date_from: new Date().setUTCHours( 0, 0, 0, 0 )
+    };
+    let doc2 = {
+      profile_id: 2,
+      user_id: 1,
+      date_from: new Date().setUTCHours( 0, 0, 0, 0 )
+    };
+
+    return ProfileFollowersHistoryBL.ensure( _.clone( doc1 ) )
+      .then( () => { return ProfileFollowersHistoryBL.ensure( _.clone( doc2 ) )} )
+      .then( () => { return ProfileFollowersHistoryBL.removeByProfileId( doc2.profile_id )} )
+      .then( () => { return ProfileFollowersHistoryBL.count()} )
+      .then( ( count ) => {
+        expect( count ).to.be.equal( 1 );
+      } )
+
   } );
 
   it( '`getActiveFollowersByProfile` returns the active followers only', () => {
 
-    let profileId = 1;
+    let dateToday = new Date();
+    let doc1 = {
+      profile_id: 1,
+      user_id: 1,
+      date_from: new Date( dateToday.setDate( dateToday.getDate() - 5 ) ).setUTCHours( 0, 0, 0, 0 ),
+      date_to: new Date( dateToday.setDate( dateToday.getDate() - 4 ) ).setUTCHours( 0, 0, 0, 0 )
+    };
+    let doc2 = {
+      profile_id: 1,
+      user_id: 1,
+      date_from: new Date( dateToday.setDate( dateToday.getDate() - 3 ) ).setUTCHours( 0, 0, 0, 0 ),
+      date_to: new Date( dateToday.setDate( dateToday.getDate() - 2 ) ).setUTCHours( 0, 0, 0, 0 )
+    };
+    let doc3 = {
+      profile_id: 1,
+      user_id: 1,
+      date_from: new Date( dateToday.setDate( dateToday.getDate() - 4 ) ).setUTCHours( 0, 0, 0, 0 )
+    };
 
-    return profileFollowersHistoryBL.removeAll()
-      .then( () => {return profileFollowersHistoryBL.getActiveFollowersByProfile( profileId )} )
+    return ProfileFollowersHistoryBL.ensure( _.clone( doc1 ) )
+      .then( () => { return ProfileFollowersHistoryBL.ensure( _.clone( doc2 ) ) } )
+      .then( () => { return ProfileFollowersHistoryBL.ensure( _.clone( doc3 ) ) } )
+      .then( () => {return ProfileFollowersHistoryBL.count()} )
+      .then( ( count ) => {
+
+        // 3 in total
+        expect( count ).to.be.equal( 3 );
+      } )
+      .then( () => { return ProfileFollowersHistoryBL.getActiveFollowersByProfile( doc1.profile_id ) } )
       .then( ( result ) => {
         expect( result ).to.exist.and.to.be.an.array;
+
+        // but only 1 active
         expect( result ).to.have.length( 1 );
-      } )
-      .catch( ( err ) => {
-        expect( err ).to.not.exist;
       } )
   } )
 
