f6fafcd8c3e13c815fa6e7805ecfebf6019faadc
Stefan Walther <Stefan.Walther@qlik.com>
2017-01-13 17:10:12 +0100

    refactor: parametrize mq-worker

--- src/api/mq/mq-worker.js
+++ src/api/mq/mq-worker.js
@@ -1,6 +1,8 @@
 const amqp = require('amqplib');
 // const amqpSugar = require('amqplib-sugar');
 // const superagent = require('superagent-promise')(require('superagent'), Promise);
+const GitHubProfile = require('./../modules/github/github.profile');
+const gitHubProfileBL = require('./../modules/profile/profile.bl');
 
 const uri = process.env.SAMMLER_RABBITMQ_URI;
 
@@ -8,16 +10,17 @@ const uri = process.env.SAMMLER_RABBITMQ_URI;
 class MqWorker {
   constructor() {
     this.init();
+    this.gitHubProfile = new GitHubProfile();
   }
 
   init() {
-    this.listenFoo();
+    this.listenProfileSyncRequested();
   }
 
-  listenFoo() {
+  listenProfileSyncRequested() {
     const ex = 'github.profile-sync';
     const exchangeType = 'topic';
-    const queueProfileSync = 'queue-profile-sync';
+    const queueProfileSync = 'queue.github.profile-sync';
 
     amqp.connect(uri)
       .then(conn => {
@@ -27,14 +30,17 @@ class MqWorker {
         return Promise.all([
           channel.assertExchange(ex, exchangeType),
           channel.assertQueue(queueProfileSync, {exclusive: false}),
-          channel.bindQueue(queueProfileSync, ex, '#'),
+          channel.bindQueue(queueProfileSync, ex, 'sync.requested'),
           channel.consume(queueProfileSync, msg => {
             // eslint-disable-next-line quotes
             console.log(" [x] %s - %s:'%s'", '#', msg.fields.routingKey, msg.content.toString());
 
             // Mark job as running
 
+
             // Run the job
+            this.gitHubProfile.getProfile()
+              .then(result => console.log('gh.getProfile.result', result));
 
             // Mark job as finished
 
