a3dba084267b34c6968d6080f928ea079faa9130
Stefan Walther <Stefan.Walther@qlik.com>
2017-01-13 17:09:25 +0100

    refactor: Remove unnecessary console.logs

--- src/api/modules/github/github.auth.js
+++ src/api/modules/github/github.auth.js
@@ -5,8 +5,10 @@ let config = null;
 
 const p = path.join(process.env.HOME, '.s5rrc');
 if (fs.existsSync(p)) {
+  console.log('OK, load file: ', p);
   config = JSON.parse(fs.readFileSync(p, 'utf8'));
 } else {
+  console.log('OK, we have to set it manually');
   config = {
     type: 'oauth',
     token: process.env.S5R_STRATEGY_GITHUB__TOKEN
