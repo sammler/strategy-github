322e04dbb76643f34481c4022e50aee84d848663
Stefan Walther <Stefan.Walther@qlik.com>
2017-01-27 21:48:26 -0500

    fix: first github.profile-sync flow

--- src/api/mq/mq-worker.js
+++ src/api/mq/mq-worker.js
@@ -4,6 +4,7 @@ const superagent = require('superagent-promise')(require('superagent'), Promise)
 const GitHubProfile = require('./../modules/github/github.profile');
 const gitHubProfileBL = require('./../modules/profile/profile.bl');
 const logger = require('./../config/context').instance().logger;
+const moment = require('moment');
 
 const uri = process.env.SAMMLER_RABBITMQ_URI;
 const SAMMLER_JOBS_SERVICE = process.env.SAMMLER_JOBS_SERVICE__URI;
@@ -41,22 +42,43 @@ class MqWorker {
 
             // Mark job as running
             const jobId = msgContent.job_id;
-            logger.silly('JobId: %s', jobId);
-
             return superagent
               .patch(SAMMLER_JOBS_SERVICE + `/v1/jobs/${jobId}/status`)
               .send({status: 'running'})
               .then(() => {
                 return this.gitHubProfile.getProfile()
-                .then(result => {
-                  logger.silly('GitHub meta: ', result.meta);
-                  return result;
-                })
-                .then(result => gitHubProfileBL.save(result))
-                .then(() => {
-                  channel.ack(msg);
-                  return Promise.resolve();
-                })
+                  .then(result => {
+                    logger.silly('GitHub meta: ', result.meta);
+                    // Todo: Something is wrong, the x-ratelimit-reset is always in the past
+                    // https://developer.github.com/v3/#rate-limiting
+                    logger.silly('Time to next reset: ', moment.utc(result.meta['x-ratelimit-reset'], 'X').format("DD-MM-YYYY HH:mm:ss"));
+                    logger.silly('Time to next reset: ', new Date(result.meta['x-ratelimit-reset'] * 1000));
+                    logger.silly('Time to next reset: ', new Date(result.meta['x-ratelimit-reset'] * 1000).toLocaleString());
+                    return result;
+                  })
+                  .then(result => gitHubProfileBL.save(result))
+                  .then(() => {
+                    logger.silly('Complete job', jobId);
+                    return superagent
+                      .patch(SAMMLER_JOBS_SERVICE + `/v1/jobs/${jobId}/status`)
+                      .send({status: 'completed'})
+                  })
+                  .then(() => {
+                    superagent
+                      .get(SAMMLER_JOBS_SERVICE + `/v1/jobs/${jobId}`)
+                      .then(result => {
+                        logger.silly('response.body', result.body);
+                        return Promise.resolve();
+                      })
+                  })
+                  .then(() => {
+                    logger.silly('OK, ack');
+                    channel.ack(msg);
+                    return Promise.resolve();
+                  })
+              })
+              .catch(err => {
+                logger.error('err', err);
               });
 
           }, {noAck: false})
